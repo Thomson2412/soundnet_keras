7661ef6edcdc019885f4aa30ef7e6dd0fb35ce52
Thomson2412 <thomasm.w.fink@gmail.com>
2021-03-18 12:31:08 +0100

    Combine two models into one whit a split output

--- soundnet.py
+++ soundnet.py
@@ -1,9 +1,8 @@
-from keras.layers import BatchNormalization, Activation, Conv1D, MaxPooling1D, ZeroPadding1D, InputLayer
-from keras.models import Sequential
+from keras.layers import BatchNormalization, Activation, Conv1D, MaxPooling1D, ZeroPadding1D, Input
+from keras.models import Model
 import numpy as np
 import librosa
 import glob
-import tensorflow as tf
 
 
 def preprocess(audio):
@@ -20,71 +19,81 @@ def load_audio(audio_file):
     return audio
 
 
-def build_model(cat_type):
+def build_model():
     model_weights = dict(np.load('models/sound8.npy', encoding="latin1", allow_pickle=True).item())
-    model_seq = Sequential()
-    model_seq.add(InputLayer(batch_input_shape=(1, None, 1)))
+    seq_layer_parameters = [{'name': 'conv1', 'num_filters': 16, 'padding': 32,
+                             'kernel_size': 64, 'conv_strides': 2,
+                             'pool_size': 8, 'pool_strides': 8},
 
-    filter_parameters = [{'name': 'conv1', 'num_filters': 16, 'padding': 32,
-                          'kernel_size': 64, 'conv_strides': 2,
-                          'pool_size': 8, 'pool_strides': 8},
+                            {'name': 'conv2', 'num_filters': 32, 'padding': 16,
+                             'kernel_size': 32, 'conv_strides': 2,
+                             'pool_size': 8, 'pool_strides': 8},
 
-                         {'name': 'conv2', 'num_filters': 32, 'padding': 16,
-                          'kernel_size': 32, 'conv_strides': 2,
-                          'pool_size': 8, 'pool_strides': 8},
+                            {'name': 'conv3', 'num_filters': 64, 'padding': 8,
+                             'kernel_size': 16, 'conv_strides': 2},
 
-                         {'name': 'conv3', 'num_filters': 64, 'padding': 8,
-                          'kernel_size': 16, 'conv_strides': 2},
+                            {'name': 'conv4', 'num_filters': 128, 'padding': 4,
+                             'kernel_size': 8, 'conv_strides': 2},
 
-                         {'name': 'conv4', 'num_filters': 128, 'padding': 4,
-                          'kernel_size': 8, 'conv_strides': 2},
+                            {'name': 'conv5', 'num_filters': 256, 'padding': 2,
+                             'kernel_size': 4, 'conv_strides': 2,
+                             'pool_size': 4, 'pool_strides': 4},
 
-                         {'name': 'conv5', 'num_filters': 256, 'padding': 2,
-                          'kernel_size': 4, 'conv_strides': 2,
-                          'pool_size': 4, 'pool_strides': 4},
+                            {'name': 'conv6', 'num_filters': 512, 'padding': 2,
+                             'kernel_size': 4, 'conv_strides': 2},
 
-                         {'name': 'conv6', 'num_filters': 512, 'padding': 2,
-                          'kernel_size': 4, 'conv_strides': 2},
+                            {'name': 'conv7', 'num_filters': 1024, 'padding': 2,
+                             'kernel_size': 4, 'conv_strides': 2}
+                            ]
 
-                         {'name': 'conv7', 'num_filters': 1024, 'padding': 2,
-                          'kernel_size': 4, 'conv_strides': 2}
-                         ]
-    if cat_type == "object":
-        filter_parameters.append(
-            {'name': 'conv8', 'num_filters': 1000, 'padding': 0, 'kernel_size': 8, 'conv_strides': 2})
-    elif cat_type == "scene":
-        filter_parameters.append(
-            {'name': 'conv8_2', 'num_filters': 401, 'padding': 0, 'kernel_size': 8, 'conv_strides': 2})
-    else:
-        raise ValueError("Type must be scene or object")
+    # Build sequential layers
+    inputs = Input(shape=(None, 1))
+    prev_layer = inputs
+    for seq_layer_parameter in seq_layer_parameters:
+        prev_layer = ZeroPadding1D(padding=seq_layer_parameter['padding'])(prev_layer)
 
-    for filter_parameter in filter_parameters:
-        model_seq.add(ZeroPadding1D(padding=filter_parameter['padding']))
-        model_seq.add(Conv1D(filter_parameter['num_filters'],
-                             kernel_size=filter_parameter['kernel_size'],
-                             strides=filter_parameter['conv_strides'],
-                             padding='valid'))
-        weights = model_weights[filter_parameter['name']]['weights'].reshape(
-            model_seq.layers[-1].get_weights()[0].shape)
-        biases = model_weights[filter_parameter['name']]['biases']
+        conv_layer = Conv1D(seq_layer_parameter['num_filters'],
+                            kernel_size=seq_layer_parameter['kernel_size'],
+                            strides=seq_layer_parameter['conv_strides'],
+                            padding='valid',
+                            name=seq_layer_parameter['name'])
 
-        model_seq.layers[-1].set_weights([weights, biases])
+        prev_layer = conv_layer(prev_layer)
+        weights = model_weights[seq_layer_parameter['name']]['weights'].reshape(conv_layer.get_weights()[0].shape)
+        biases = model_weights[seq_layer_parameter['name']]['biases']
+        conv_layer.set_weights([weights, biases])
 
-        if 'conv8' not in filter_parameter['name']:
-            gamma = model_weights[filter_parameter['name']]['gamma']
-            beta = model_weights[filter_parameter['name']]['beta']
-            mean = model_weights[filter_parameter['name']]['mean']
-            var = model_weights[filter_parameter['name']]['var']
+        gamma = model_weights[seq_layer_parameter['name']]['gamma']
+        beta = model_weights[seq_layer_parameter['name']]['beta']
+        mean = model_weights[seq_layer_parameter['name']]['mean']
+        var = model_weights[seq_layer_parameter['name']]['var']
+        batch_norm = BatchNormalization()
+        prev_layer = batch_norm(prev_layer)
+        batch_norm.set_weights([gamma, beta, mean, var])
 
-            model_seq.add(BatchNormalization())
-            model_seq.layers[-1].set_weights([gamma, beta, mean, var])
-            model_seq.add(Activation('relu'))
-        if 'pool_size' in filter_parameter:
-            model_seq.add(MaxPooling1D(pool_size=filter_parameter['pool_size'],
-                                       strides=filter_parameter['pool_strides'],
-                                       padding='valid'))
+        prev_layer = Activation('relu')(prev_layer)
 
-    return model_seq
+        if 'pool_size' in seq_layer_parameter:
+            prev_layer = MaxPooling1D(pool_size=seq_layer_parameter['pool_size'],
+                                      strides=seq_layer_parameter['pool_strides'],
+                                      padding='valid')(prev_layer)
+
+    # Build split output layers
+    object_output_layer = ZeroPadding1D(padding=0)(prev_layer)
+    conv_layer = Conv1D(1000, kernel_size=8, strides=2, padding='valid', name='conv8')
+    object_output_layer = conv_layer(object_output_layer)
+    weights = model_weights['conv8']['weights'].reshape(conv_layer.get_weights()[0].shape)
+    biases = model_weights['conv8']['biases']
+    conv_layer.set_weights([weights, biases])
+
+    scene_output_layer = ZeroPadding1D(padding=0)(prev_layer)
+    conv_layer = Conv1D(401, kernel_size=8, strides=2, padding='valid', name='conv8_2')
+    scene_output_layer = conv_layer(scene_output_layer)
+    weights = model_weights['conv8_2']['weights'].reshape(conv_layer.get_weights()[0].shape)
+    biases = model_weights['conv8_2']['biases']
+    conv_layer.set_weights([weights, biases])
+
+    return Model(inputs=inputs, outputs=[object_output_layer, scene_output_layer])
 
 
 def predictions_to_categories(prediction, cat):
@@ -96,9 +105,8 @@ def predictions_to_categories(prediction, cat):
 
 
 if __name__ == '__main__':
-    print(tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None))
-    model_object = build_model("object")
-    model_scene = build_model("scene")
+    model_object = build_model()
+    model_object.summary()
     with open('categories/categories_imagenet.txt', 'r') as f:
         object_categories = f.read().split('\n')
     with open('categories/categories_places2.txt', 'r') as f:
@@ -114,10 +122,9 @@ if __name__ == '__main__':
                 break
 
     for filename, file in files.items():
-        plain_object_prediction = model_object.predict(file)
-        object_pred_cat = predictions_to_categories(plain_object_prediction, object_categories)
+        plain_prediction = model_object.predict(file)
+        object_pred_cat = predictions_to_categories(plain_prediction[0], object_categories)
         print("OBJECT {}: {}".format(filename, object_pred_cat))
 
-        plain_scene_prediction = model_scene.predict(file)
-        scene_pred_cat = predictions_to_categories(plain_scene_prediction, scene_categories)
+        scene_pred_cat = predictions_to_categories(plain_prediction[1], scene_categories)
         print("SCENE {}: {}".format(filename, scene_pred_cat))
